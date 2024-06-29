61af2df0f7e27eddf1e27c215b3f97e2a6436bc5
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-06-29 16:24:35 +0100

    Create function for player choice and variables for both scores

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+// Variables For Player Score and Computer Score
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+
+
+
+
+
+// Generate computer choice
 function getComputerChoice() {
     let randomNumber = Math.ceil(Math.random() * 3);
 
-    switch(randomNumber) {
+    switch (randomNumber) {
         case 1:
             return 'rock'
         case 2:
@@ -11,4 +24,15 @@ function getComputerChoice() {
         default:
             'Erro na geração de um numero random'
     }
+}
+
+// Takes player choice
+function getHumanChoice() {
+    let humanChoice = '';
+    /* do { */
+        humanChoice = prompt('Choose one: rock, paper, scissors');
+    /* }
+    while (humanChoice != 'rock' || humanChoice != 'paper' || humanChoice != 'scissors') */
+
+    return humanChoice;
 }

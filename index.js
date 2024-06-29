49470ad3a52e46503f75c0115c4f06a27e670699
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-06-29 16:37:05 +0100

    Create function to play

--- index.js
+++ index.js
@@ -4,11 +4,51 @@ let computerScore = 0;
 
 
 
+// Play Round
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === 'rock' && computerChoice === 'rock') {
+        console.log('Draw');
+        return
 
+    } else if(humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You lose! Paper beats Rock');
+        computerScore++
+        return
 
+    } else if(humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You Win! Rock beats Scissors');
+        humanScore++
+        return
 
+    } else if(humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You Win! Paper beats Rock');
+        humanScore++
+        return
 
+    } else if(humanChoice === 'paper' && computerChoice === 'paper') {
+        console.log('Draw');
+        return
 
+    } else if(humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('You lose! Scissors beats Paper');
+        computerScore++
+        return
+
+    } else if(humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You lose! Rock beats Scissors');
+        computerScore++
+        return
+
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win. Scissors beats Paper');
+        humanScore++
+        return
+
+    } else if(humanChoice === 'scissors' && computerChoice === 'scissors') {
+        console.log('Draw');
+        return
+    }
+}
 
 // Generate computer choice
 function getComputerChoice() {
